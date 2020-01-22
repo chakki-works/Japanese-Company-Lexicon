53c690b620bb97055308a93c888b774bd336c8b2
Bramble Xu <liangxu006@gmail.com>
2020-01-23 08:11:47 +0900

    Alias generation script

---
+++ generate_alias.sh
@@ -0,0 +1,15 @@
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+echo ${green}=== Company Alias Generation ===${reset}
+
+echo "Convert CSV to JSONL..."
+python tools/covert_csv2jsonl.py
+
+echo "Generate alias..."
+python tools/alias_generation.py
+
+echo "Filter alias..."
+python tools/jcl_filter.py
+
+echo ${green}=== Company Alias Generation Done ===${reset}
